f8c555eacb2e0aa5646558867e50f4dc2a7d2d0f
Aparna devi chinnu <aparnadevi.rajavel@thoughtworks.com>
2021-04-23 14:29:57 +0530

    [Aparna]Add. Created a method setInitialPosition in Rover class

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -3,4 +3,8 @@ public class Rover
     private int x;
     private int y;
     private char dir;
+
+    public void setInitialPosition(int i, int i1, char n)
+    {
+    }
 }
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -5,8 +5,11 @@ public class RoverTest
     @Test
     public void shouldReturnTheCurrentPositionAfterGivenMovements()
     {
-        new Plateau();
+        Plateau plateau=new Plateau();
+        plateau.setMax_x(5);
+        plateau.setMax_y(5);
        Rover rover = new Rover();
+       rover.setInitialPosition(1,2,'N')
 
     }
 }
