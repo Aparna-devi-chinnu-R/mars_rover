9bbe9532daf4c805008a76d655110888346129b4
Aparna devi chinnu <aparnadevi.rajavel@thoughtworks.com>
2021-04-23 15:33:36 +0530

    [Aparna]Add. Created a method commands in Rover to process commands

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -4,7 +4,19 @@ public class Rover
     private int y;
     private char dir;
 
-    public void setInitialPosition(int i, int i1, char n)
+    public void setInitialPosition(int x, int y, char dir)
     {
+        this.x=x;
+        this.y=y;
+        this.dir=dir;
+    }
+
+    public void commands(String command)
+    {
+        for(int i=0;i<command.length();i++)
+        {
+            char single_command = command.charAt(i);
+
+        }
     }
 }
--- src/test/java/RoverTest.java
+++ src/test/java/RoverTest.java
@@ -8,8 +8,9 @@ public class RoverTest
         Plateau plateau=new Plateau();
         plateau.setMax_x(5);
         plateau.setMax_y(5);
-       Rover rover = new Rover();
-       rover.setInitialPosition(1,2,'N')
+        Rover rover = new Rover();
+        rover.setInitialPosition(1,2,'N');
+        rover.commands("LMLMLMLMM");
 
     }
 }
