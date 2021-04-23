d76bbfa8a7c804af9733588942e39ec86fe620c2
Aparna devi chinnu <aparnadevi.rajavel@thoughtworks.com>
2021-04-23 15:47:01 +0530

    [Aparna]Add. Logic to turn and move according to commands

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -16,7 +16,55 @@ public class Rover
         for(int i=0;i<command.length();i++)
         {
             char single_command = command.charAt(i);
-
+            if(single_command == 'L')
+            {
+                turnLeft();
+            }
+            else if(single_command == 'R')
+            {
+                turnRight();
+            }
+            else if(single_command == 'M')
+            {
+                move();
+            }
         }
     }
+
+    private void move()
+    {
+        if(dir =='N')
+        {
+            this.x++;
+        }
+        else if(dir == 'S')
+        {
+            this.x--;
+        }
+        else if(dir == 'E')
+        {
+            this.y++;
+        }
+        else
+        {
+            this.y--;
+        }
+    }
+
+    private void turnRight()
+    {
+        if(dir == 'N'){dir = 'E';}
+        else if(dir == 'W'){dir='N';}
+        else if(dir == 'S'){dir ='W';}
+        else{dir = 'S';}
+    }
+
+    private void turnLeft()
+    {
+        if(dir == 'N'){dir = 'W';}
+        else if(dir == 'W'){dir='S';}
+        else if(dir == 'S'){dir ='E';}
+        else{dir = 'N';}
+    }
+
 }
